ce8fbe8e20d2c8dbfe6e39e4d33e06fbd7140c7b
zhmyh1337 <70749157+zhmyh1337@users.noreply.github.com>
2020-12-21 17:37:23 +0300

    Initial structure.

--- Lib/src/graph.h
+++ Lib/src/graph.h
@@ -2,5 +2,27 @@
 
 namespace graph
 {
-	
+	/// <summary>
+	/// Weighted graph.
+	/// </summary>
+	/// <typeparam name="_VertexType">Type of vertices.</typeparam>
+	/// <typeparam name="_WeightType">Type of weights.</typeparam>
+	template<class _VertexType, class _WeightType = void>
+	class Graph
+	{
+	public:
+		using VertexType = _VertexType;
+		using WeightType = _WeightType;
+	};
+
+	/// <summary>
+	/// Unweighted graph.
+	/// </summary>
+	/// <typeparam name="_VertexType">Type of vertices.</typeparam>
+	template<class _VertexType>
+	class Graph<_VertexType, void>
+	{
+	public:
+		using VertexType = _VertexType;
+	};
 }
--- Samples/Application1/src/main.cpp
+++ Samples/Application1/src/main.cpp
@@ -1,4 +1,5 @@
 #include <graph.h>
+#include <vector>
 
 int main()
 {
